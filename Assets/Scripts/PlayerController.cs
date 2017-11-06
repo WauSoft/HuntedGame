a5408d5a55dfc79af5fb2b9478cd25e7fa3ff12c
Niko Lepka <slench102+git@gmail.com>
2017-11-06 21:55:49 +0100

    add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    private float SpeedOffset
+    {
+        get { return Time.deltaTime * 5; }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDir = Input.GetAxis("Horizontal") * SpeedOffset;
+        float zDir = Input.GetAxis("Vertical") * SpeedOffset;
+        transform.Translate(xDir, 0, zDir);
+    }
+}
