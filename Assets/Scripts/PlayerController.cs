663cf144325a19898713fc752846dd693736a66f
Niko Lepka <slench102+git@gmail.com>
2017-11-08 20:08:10 +0100

    Add Forward and Backward as valid Directions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public enum Direction { Left, Right }
+public enum Direction { Left, Right, Forward, Backward }
 
 public class PlayerController : MonoBehaviour
 {
